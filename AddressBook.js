4017dbd1d4d792c844819e27f413004d17faec8e
TanyaMehta649 <tanyamehta649@gmail.com>
2025-01-23 14:31:10 +0530

    File added

--- AddressBook.js
+++ AddressBook.js
@@ -1,43 +1,43 @@
-"use strict";
-const readline = require("readline");
-const rl = readline.createInterface({
+var readline = require("readline");
+var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-class AddressBook {
-    constructor() {
+var AddressBook = /** @class */ (function () {
+    function AddressBook() {
         this.contacts = [];
     }
-    addContact(contact) {
+    AddressBook.prototype.addContact = function (contact) {
         this.contacts.push(contact);
         console.log("Contact added successfully.");
-    }
-    viewContacts() {
+    };
+    AddressBook.prototype.viewContacts = function () {
         if (this.contacts.length === 0) {
             console.log("No contacts found.");
         }
         else {
             console.log("Address Book Contacts:");
-            this.contacts.forEach((contact, index) => {
-                console.log(`${index + 1}. ${contact.firstName} ${contact.lastName}`);
-                console.log(`   Address: ${contact.address}, ${contact.city}, ${contact.state}, ${contact.zip}`);
-                console.log(`   Phone: ${contact.phoneNo}`);
-                console.log(`   Email: ${contact.email}`);
+            this.contacts.forEach(function (contact, index) {
+                console.log("".concat(index + 1, ". ").concat(contact.firstName, " ").concat(contact.lastName));
+                console.log("   Address: ".concat(contact.address, ", ").concat(contact.city, ", ").concat(contact.state, ", ").concat(contact.zip));
+                console.log("   Phone: ".concat(contact.phoneNo));
+                console.log("   Email: ".concat(contact.email));
             });
         }
-    }
-}
-const addressBook = new AddressBook();
-const addNewContact = () => {
-    rl.question("Enter First Name: ", (firstName) => {
-        rl.question("Enter Last Name: ", (lastName) => {
-            rl.question("Enter Address: ", (address) => {
-                rl.question("Enter City: ", (city) => {
-                    rl.question("Enter State: ", (state) => {
-                        rl.question("Enter ZIP Code: ", (zip) => {
-                            rl.question("Enter Phone Number: ", (phoneNo) => {
-                                rl.question("Enter Email: ", (email) => {
-                                    const contact = { firstName, lastName, address, city, state, zip, phoneNo, email };
+    };
+    return AddressBook;
+}());
+var addressBook = new AddressBook();
+var addNewContact = function () {
+    rl.question("Enter First Name: ", function (firstName) {
+        rl.question("Enter Last Name: ", function (lastName) {
+            rl.question("Enter Address: ", function (address) {
+                rl.question("Enter City: ", function (city) {
+                    rl.question("Enter State: ", function (state) {
+                        rl.question("Enter ZIP Code: ", function (zip) {
+                            rl.question("Enter Phone Number: ", function (phoneNo) {
+                                rl.question("Enter Email: ", function (email) {
+                                    var contact = { firstName: firstName, lastName: lastName, address: address, city: city, state: state, zip: zip, phoneNo: phoneNo, email: email };
                                     addressBook.addContact(contact);
                                     mainMenu();
                                 });
@@ -49,14 +49,9 @@ const addNewContact = () => {
         });
     });
 };
-const mainMenu = () => {
-    console.log(`
-     ADDRESS BOOK MENU
-    1. Add a new contact
-    2. View all contacts
-    3. Exit
-    `);
-    rl.question("Choose an option (1-3): ", (choice) => {
+var mainMenu = function () {
+    console.log("\n     ADDRESS BOOK MENU\n    1. Add a new contact\n    2. View all contacts\n    3. Exit\n    ");
+    rl.question("Choose an option (1-3): ", function (choice) {
         switch (choice.trim()) {
             case "1":
                 addNewContact();
